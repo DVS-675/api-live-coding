ccf25b68df3edd1a07e3c95bd4c0edac6a6c58e6
DVS-675 <dmitryvs20170@gmail.com>
2023-04-04 12:06:39 +0500

    Выносит форму входа в отдельный компонент

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { addTodo, deleteTodo, getTodos } from "./api.js";
-
-const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
+import { renderLoginComponent } from "./components/login-component.js";
 let token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k37k3bk3cg3c03ck";
 let tasks = [];
 
@@ -16,27 +15,13 @@ const fetchTodosAndRender = () => {
 const renderApp = () => {
   const appEl = document.getElementById("app");
   if (!token) {
-    const appHtml = `<h1>Список задач</h1>
-                    <div class="form">
-                        <h3 class="form-title">Форма входа</h3>
-                        <div class="form-row">
-                            Логин
-                            <input type="text" id="login-input" class="input" />
-                            <br />
-                            Пароль
-                            <input type="text" id="login-input" class="input" />
-                        </div>
-                        <br />
-                        <button class="button" id="login-button">Войти</button>
-                        </div>`;
-
-    appEl.innerHTML = appHtml;
-
-    document.getElementById("login-button").addEventListener("click", () => {
-      token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k37k3bk3cg3c03ck";
-      fetchTodosAndRender();
+    renderLoginComponent({
+      appEl,
+      setToken: (newToken) => {
+        token = newToken;
+      },
+      fetchTodosAndRender,
     });
-
     return;
   }
 
